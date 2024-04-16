88365eb4447fa7cf7b1e35f6176b65e5b9f24ddf
Nataliamroz123 <163434752+Nataliamroz123@users.noreply.github.com>
2024-04-16 18:51:29 +0200

    wip: add new menu function, add user search

--- models/data_source.py
+++ models/data_source.py
@@ -2,7 +2,8 @@ users: list = [
 
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500},
     {"name": "Hubert", "surname": "Sybilski", "posts": 123456},
-    {"name": "Adrian", "surname": "Dobrzański", "posts": 3}
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 3},
+    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666}
 
 
 ]
